ab4893bc40dfdcbef2f125d7b0320fdb1dbff14c
Nicholas Niles <96033501+nick1076@users.noreply.github.com>
2024-07-18 18:15:10 -0400

    KiCad

---
+++ mk2/mk2.ino
@@ -0,0 +1,44 @@
+
+const int firingIndicator = 4;
+const int mosfetGate = 3;
+const int firingInput = 2;
+
+//Timing (ms)
+const int delayMosfetToggle = 100;
+const int delayIndicatorToggle = 100;
+const int firingCooldown = 1000;
+
+bool canFire = false;
+
+void setup() {
+  pinMode(firingIndicator, OUTPUT);
+  pinMode(mosfetGate, OUTPUT);
+  pinMode(firingInput, INPUT);
+
+  digitalWrite(firingIndicator, LOW);
+  digitalWrite(mosfetGate, LOW);
+
+  digitalWrite(firingIndicator, HIGH);
+  delay(500);
+  digitalWrite(firingIndicator, LOW);
+  canFire = true;
+}
+
+void loop() {
+  if (digitalRead(firingInput) == HIGH && canFire){
+    Fire();
+    canFire = false;
+    delay(firingCooldown);
+    canFire = true;
+  }
+}
+
+void Fire(){
+  digitalWrite(firingIndicator, HIGH);
+  digitalWrite(mosfetGate, HIGH);
+  delay(delayMosfetToggle);
+  digitalWrite(mosfetGate, LOW);
+  delay(delayIndicatorToggle);
+  digitalWrite(firingIndicator, LOW);
+
+}
